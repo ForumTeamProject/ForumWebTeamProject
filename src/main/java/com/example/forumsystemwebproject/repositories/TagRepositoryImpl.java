1a8b1012a1dbf95ff5c8bc47cc9afb24b367484f
PetkoDeyanov <petkopetkov1001@gmail.com>
2023-11-03 20:12:09 +0200

    Fixing a getByContent method in TagRepositoryImpl

--- src/main/java/com/example/forumsystemwebproject/repositories/TagRepositoryImpl.java
+++ src/main/java/com/example/forumsystemwebproject/repositories/TagRepositoryImpl.java
@@ -6,6 +6,7 @@ import com.example.forumsystemwebproject.repositories.contracts.TagRepository;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,13 +15,14 @@ import java.util.List;
 public class TagRepositoryImpl implements TagRepository {
     private final SessionFactory sessionFactory;
 
+    @Autowired
     public TagRepositoryImpl(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
 
     public List<Tag> getAll() {
         try (Session session = sessionFactory.openSession()) {
-            Query<Tag> query = session.createQuery("from Tag", Tag.class);
+            Query<Tag> query = session.createQuery("FROM Tag", Tag.class);
             return query.list();
         }
     }
@@ -39,11 +41,16 @@ public class TagRepositoryImpl implements TagRepository {
     @Override
     public Tag getByContent(String content) {
         try (Session session = sessionFactory.openSession()) {
-            Tag result = session.get(Tag.class, content);
-            if (result == null) {
+            Query<Tag> query = session.createQuery("FROM Tag WHERE content = :content", Tag.class);
+            query.setParameter("content", content);
+
+            List<Tag> result = query.list();
+
+            if (result.isEmpty()) {
                 throw new EntityNotFoundException("Tag", "content", content);
             }
-            return result;
+
+            return result.get(0);
         }
     }
 
@@ -73,8 +80,8 @@ public class TagRepositoryImpl implements TagRepository {
 
     @Override
     public void delete(int id) {
-        Tag tagToRemove = getById(id);
         try (Session session = sessionFactory.openSession()) {
+            Tag tagToRemove = getById(id);
             session.beginTransaction();
             session.remove(tagToRemove);
             session.getTransaction().commit();
